ce8a05ff67983665237ee9e42dcad1314c4c4678
ThibaultPougin <t.pougin@gmail.com>
2022-02-25 12:14:56 +0100

    Notice de configuration

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Gmail_API
 
+1. Creer un nouveau projet sur Google Developer Console (https://console.cloud.google.com/getting-started).
+2. Créer les identifiants OAuth pour identifier notre application sur les serveurs OAuth (Create credentials => OAuth client ID).
+3. Configurerl'écran de consentement (Configure consent screen) pour créer un Client ID et un Client Secret.
+4. Indiquer l'adresse de redirection suivante : https://developers.google.com/oauthplayground.
+5. Aller sur https://developers.google.com/oauthplayground, indiquer son Client ID et Client Secret.
+6. Authoriser Gmail API v1 => https://mail.google.com/ et confirmer l'accès au compte google.
+7. Récupérer le refresh token (Exchange authorization code for tokens).
--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 import dotenv from "dotenv";
 import nodemailer from "nodemailer";
-import { google } from "googleapis";
+import { google } from "googleapis"; // Pour générer dynamiquement un access token
 import { getDate } from './getDate.js';
 import { getWeather } from './getWeather.js';
 
 dotenv.config();
 
-// const weather = getWeather().then(result =>{
-//   return result;
-// });
+let weather = await getWeather(); // Récupère l'information sur la météo (string)
 
-let weather = await getWeather();
-
+// Récupère les variables d'environnement
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
+// Créé un client OAuth
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
   REDIRECT_URI
 );
 
+// Créé un access token à partir du refresh token
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 async function sendMail() {
@@ -30,6 +29,7 @@ async function sendMail() {
   try {
 const accessToken = await oAuth2Client.getAccessToken();
 
+// Créé un objet transporter qui contient le service utilisé (Gmail) et les identifiants nécéssaires à l'authentification
 const transport = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -42,6 +42,7 @@ const transport = nodemailer.createTransport({
   }
 });
 
+// Définit les options de l'e-mail (expéditeur, destinataire, objet, contenu, pièce(s)-jointe(s), ...)
 const mailOptions = {
   from: 'API MFA <api-mfa@geoplc.com>',
   to: 'tpougin@geoplc.com',
@@ -53,6 +54,7 @@ const mailOptions = {
   }]
 };
 
+// Envoi du mail
 const result = await transport.sendMail(mailOptions);
 return result;
 
